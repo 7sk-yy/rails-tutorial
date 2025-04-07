4f70938bdc21a607998122b37132bbc64528754b
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-04-07 14:32:12 +0900

    3.4.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
@@ -16,5 +15,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root "application#hello"
+  root "static_pages#home"
 end
