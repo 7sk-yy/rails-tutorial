9a6afd4ec2cd0d8decad2ee1d34767b9d353a4cb
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-04-16 15:25:07 +0900

    8.1.3

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   resources :users
 
   get "/login", to: "sessions#new"
-  post "/login", to: "sesisons#create"
-  delete "/logout", to: "sesisons#destroy"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
