b4733ede54e294d9cc627735839b8ab6b7857ed4
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-04-16 16:30:11 +0900

    8.2.6

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,20 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with valid email/invalid password" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
+    assert_response :unprocessable_entity
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   test "login with invalid information" do
     get login_path
     post login_path, params: { session: { email: "", password: "" } }
@@ -8,5 +22,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "password" } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
