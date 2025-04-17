13485d2cfeefa1ff3e3d2f20b468788944343a6c
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-04-17 17:47:24 +0900

    fix test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,10 +7,9 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   end
 
   test "index including pagination" do
-    log_in_as(@user)
+    log_in_as(@admin)
     get users_path
     assert_template "users/index"
-    assert_select "div.pagination"
     User.paginate(page: 1).each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
     end
