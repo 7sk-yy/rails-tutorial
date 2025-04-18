7e2e2d150647d814d2e1a4b37f43391485fb7faa
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-04-18 11:34:29 +0900

    12.1.3

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
 
   resources :account_activations, only: [ :edit ]
-  resources :password_reset,      only: [ :new, :create, :edit, :update ]
+  resources :password_resets,     only: [ :new, :create, :edit, :update ]
 end
