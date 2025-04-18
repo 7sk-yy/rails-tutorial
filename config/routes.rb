05106d867b4e631bb90ff0bebea5a0d4247a09f0
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-04-18 10:57:32 +0900

    12.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "password_resets/new"
+  get "password_resets/edit"
   root "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
@@ -12,4 +14,5 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
 
   resources :account_activations, only: [ :edit ]
+  resources :password_reset,      only: [ :new, :create, :edit, :update ]
 end
