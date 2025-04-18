6ddafff2f3b4c5be8ac2e0e921b4095cb4e854ca
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-04-18 10:46:23 +0900

    11.3.3

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -58,7 +58,6 @@ class LogoutTest < Logout
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
-    delete logout_path
   end
 
   test "redirect after logout" do
@@ -67,12 +66,17 @@ class LogoutTest < Logout
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
 end
 
 class RememberingTest < UsersLogin
   test "login with remembering" do
     log_in_as(@user, remember_me: "1")
-    assert_equal cookies[:remember_token], assigns(:user).remember_token
+    assert_not cookies[:remember_token].blank?
   end
 
   test "login without remembering" do
