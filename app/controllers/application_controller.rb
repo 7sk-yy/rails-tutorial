d29f21f1a01f4a4d5382c69e737120e93f1384f0
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-04-21 11:17:32 +0900

    13.3.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,12 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def hello
-    render html: "hello world!¡"
-  end
-
-  def goodbye
-    render html: "goodbye world!"
-  end
+  private
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
 end
