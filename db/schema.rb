06c1ad3d0baf18962c000bc99395dde838b0bbbf
Takumi Tosaki <takumi.tosaki@giftee.co>
2025-04-21 09:46:42 +0900

    13.1.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_18_015420) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_21_003233) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -26,4 +34,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_18_015420) do
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "microposts", "users"
 end
